2aa51ef0e4bf7742ca5358567d778fe49d3f89fd
Rihab Al-yasiri <rihabal-yasiri@Rihabs-Air.fritz.box>
2020-01-19 20:45:02 +0100

    Google OAuth with server

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const passport = require("passport"); //to help express making oauth
+const GoogleStrategy = require("passport-google-oauth20").Strategy; //each platform has its own strategy
+const keys = require("./config/keys");
+
+const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken: ", accessToken);
+      console.log("refreshToken: ", refreshToken);
+      console.log("profile: ", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
+//express make routing in Node easier
+//Route Handler
+// app.get("/", (req, res) => {
+//   res.send({ hi: "hello" });
+// });
+
+//hosting server on port 5000
+const PORT = process.env.PORT || 5000; //port from heoku
+app.listen(PORT);
